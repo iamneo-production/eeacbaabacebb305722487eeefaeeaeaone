6483cd1dd17c2ca1ab9b749fda51821b9cea6c91
Iamneo <techadmin@iamneo.ai>
2023-10-04 06:33:01 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -4,18 +4,18 @@ function calculateScore(){
     let answer_for_q1 = document.getElementsByName("France");
     
     for(var i = 0; i < answer_for_q1.length; i++){
-        if(answer_for_q1[i].checked){
+        if(answer_for_q1[i].checked && answer_for_q1[i].innerHTML ==="Paris" ){
             total_score += 1;
         }
     }
    
     let answer_for_q2 = document.getElementsByName("Painting");
     for(var i = 0; i < answer_for_q2.length; i++){
-        if(answer_for_q2[i].checked){
+        if(answer_for_q2[i].checked && answer_for_q2[i].innerHTML ==="Le" ){
             total_score += 1;
         }
     }
-    
+
     let answer_for_q3 = document.getElementsByName("Planet");
     for(var i = 0; i < answer_for_q3.length; i++){
         if(answer_for_q3[i].checked){
