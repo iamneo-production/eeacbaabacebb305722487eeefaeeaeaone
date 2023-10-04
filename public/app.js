680b212594718c9580933272c811662a154f074c
Iamneo <techadmin@iamneo.ai>
2023-10-04 06:27:47 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -15,7 +15,7 @@ function calculateScore(){
             total_score += 1;
         }
     }
-
+    
     let answer_for_q3 = document.getElementsByName("Planet");
     for(var i = 0; i < answer_for_q3.length; i++){
         if(answer_for_q3[i].checked){
