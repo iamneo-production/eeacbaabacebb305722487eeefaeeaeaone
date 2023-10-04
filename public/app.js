d11b34819872f0886758071575f35a08f0e866cb
Iamneo <techadmin@iamneo.ai>
2023-10-04 06:22:47 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,19 +1,26 @@
 function calculateScore(){
     let total_score = 0
     
-    let answer_for_q1 = document.getElementsByName("France").value;
-    if(answer_for_q1 === "Paris"){
-        total_score+= 1
+    let answer_for_q1 = document.getElementsByName("France");
+    
+    for(var i = 0; i < answer_for_q1.length; i++){
+        if(answer_for_q1[i].checked){
+            total_score += 1;
+        }
+    }
+   
+    let answer_for_q2 = document.getElementsByName("Painting");
+    for(var i = 0; i < answer_for_q2.length; i++){
+        if(answer_for_q2[i].checked){
+            total_score += 1;
+        }
     }
 
-    let answer_for_q2 = document.getElementsByName("Painting").value;
-    if(answer_for_q2 === "Pablo Picasso"){
-        total_score+= 1
-    }
-
-    let answer_for_q3 = document.getElementsByName("Planet").value;
-    if(answer_for_q3 === "Mars"){
-        total_score+= 1
+    let answer_for_q3 = document.getElementsByName("Planet");
+    for(var i = 0; i < answer_for_q3.length; i++){
+        if(answer_for_q3[i].checked){
+            total_score += 1;
+        }
     }
 
     document.getElementById("result").innerHTML = "You scored "+ total_score+" out of 3 correct!"
